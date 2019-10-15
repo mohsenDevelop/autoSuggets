6ac020bf967b82a86326726d32e8ce51a71344fd
mohsenDevelop <mohsen.kermanifar@yahoo.com>
2019-10-15 22:39:05 +0330

    make the AutoSuggest component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AutoSuggest from "./components/AutoSuggest";
 
 function App() {
+
+  const [items, useItems] = useState([
+    'Tehran',
+    'Shiraz',
+    'Ahvaz',
+    'Rasht',
+    'Kerman',
+    'Esfehan'
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <AutoSuggest items={items} />
     </div>
   );
 }
---
+++ src/components/AutoSuggest.jsx
@@ -0,0 +1,61 @@
+import React, { useState, Fragment } from 'react'
+
+
+const AutoSuggest = ({ items }) => {
+
+    const [text, setText] = useState("");
+    const [filtered, setFiltered] = useState([]);
+
+    const _onClick = () => {
+        setText(filtered && filtered.length ? filtered[0] : "");
+    }
+
+    const renderItems = () => {
+        return items.map((i, index) => <li onClick={_onClick} key={index} style={{ backgroundColor: "#DCDCDC", marginBottom: "3px", padding: "5px" }}>{i}</li>);
+    }
+
+    const onChange = e => {
+        setText(e.target.value);
+
+        //Filter item
+        const itemFilter = items.filter(i => {
+            const regex = new RegExp(text, 'gi');
+
+            return i.toLowerCase().match(regex);
+        });
+
+        setFiltered(itemFilter);
+
+    }
+
+
+
+    return (
+        <div className="card bg-light" style={{ width: "50%", margin: "50px auto" }}>
+            <form className="form" >
+                <input
+                    type="text"
+                    value={text}
+                    name="text"
+                    placeholder="Search City ..."
+                    onChange={onChange}
+                />
+            </form>
+
+            <div>
+                <ul className="list">
+                    {text && text.length ?
+                        filtered.map((f, index) => <li onClick={_onClick} key={index} style={{ backgroundColor: "#DCDCDC", marginBottom: "3px", padding: "5px" }}>{f}</li>)
+                        : renderItems()}
+                </ul>
+            </div>
+        </div>
+    )
+
+}
+
+
+
+
+
+export default AutoSuggest;
